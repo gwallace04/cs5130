b878a3977cd7faf58c7049c93b7d913dddce731a
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-09-08 19:13:43 -0500

    Save the plot instead of outputing it

--- plot.py
+++ plot.py
@@ -4,11 +4,13 @@ import math
 
 from scipy.special import gamma
 
-x = np.linspace(0, 10, 1000)
+x = np.linspace(1, 10, 1000)
 
 plt.plot(x, x, 'r', label='$n$') 
 plt.plot(x, 2 ** x, 'g', label='$2^n$')
 plt.plot(x, x * np.log(x), 'b', label='$n\lg n$')
 
 plt.legend()
-plt.show()
+#plt.show()
+
+plt.savefig('functions.png', bbox_inches='tight')
